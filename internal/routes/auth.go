9d76bccdbd7fb3a4dad5624389c6d7236b0c40b4
abbasmortazavi <abbassmortazavi@gmail.com>
2026-09-12 08:51:04 +0330

    feat(userRegister): add register method

---
+++ internal/routes/auth.go
@@ -0,0 +1,44 @@
+package routes
+
+import (
+	"backend/internal/utils"
+	"encoding/json"
+	"net/http"
+)
+
+func handelRegister(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		utils.JSON(w, http.StatusBadRequest, err.Error(), false, nil)
+		return
+	}
+	if req.Name == "" || req.Email == "" || req.Password == "" {
+		utils.JSON(w, http.StatusBadRequest, "name or email or password is required", false, nil)
+		return
+	}
+
+	exitUser, _ := models.GetUserByEmail(req.Email)
+	if exitUser != nil {
+		utils.JSON(w, http.StatusConflict, "This Email is in used!", false, nil)
+		return
+	}
+
+	hashPassword, err := utils.HashPassword(req.Password)
+	if err != nil {
+		utils.JSON(w, http.StatusInternalServerError, err.Error(), false, nil)
+		return
+	}
+
+	user, err := models.CreateUser(req.Name, req.Email, hashPassword)
+	if err != nil {
+		utils.JSON(w, http.StatusInternalServerError, err.Error(), false, nil)
+		return
+	}
+	utils.JSON(w, http.StatusCreated, user, true, nil)
+}
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,5 +7,7 @@ import (
 func RegisterRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /api/health", handelHealth)
+
+	mux.HandleFunc("POST /api/register", handelRegister)
 	return mux
 }
